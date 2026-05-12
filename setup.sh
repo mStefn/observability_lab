571af246a28486da73658fcc00640e6802ea4058
Maciej Stefanowicz <stefanstefanskivip@gmail.com>
2026-05-12 19:51:23 +0200

    modified setup.sh scirpt so loki contaner will work

--- setup.sh
+++ setup.sh
@@ -2,18 +2,18 @@
 
 echo "Przygotowywanie srodowiska Observability Lab..."
 
-# Tworzenie struktury katalogow przy uzyciu sudo
-echo "Tworzenie struktury katalogow..."
-sudo mkdir -p ./data/grafana ./data/prometheus ./data/postgres
 
-# Ustawianie uprawnien (Grafana ID: 472, Prometheus ID: 65534, Postgres ID: 999)
+echo "Tworzenie struktury katalogow..."
+sudo mkdir -p ./data/grafana ./data/prometheus ./data/postgres ./data/loki
+
+
 echo "Ustawianie uprawnien zapisu dla kontenerow..."
 sudo chown -R 472:472 ./data/grafana
 sudo chown -R 65534:65534 ./data/prometheus
 sudo chown -R 999:999 ./data/postgres
+sudo chown -R 10001:10001 ./data/loki  
 
-# Opcjonalne: upewnienie sie, ze foldery maja odpowiednie tryby zapisu
-sudo chmod -R 775 ./data/grafana ./data/prometheus ./data/postgres
 
-echo "Gotowe!"
-echo "Uruchom projekt: docker compose up -d"
+sudo chmod -R 775 ./data/grafana ./data/prometheus ./data/postgres ./data/loki
+
+echo "Gotowe! Teraz Loki powinien wstac bez problemu."
